d543c9f18c2a53ee5ebc84cd55e5aab62a93eb51
ryanleeallred <ryanleeallred@gmail.com>
2013-11-19 12:24:57 -0700

    commits changes to app.js

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var path = require('path');
 var DataTemplate = require('./DataTemplate.js');
 var app = express();
 
+var dbName = 'EducationData';
+var dbUser = 'root';
+var dbPass = null;
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -21,16 +25,52 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-var sequelize = new Sequelize('EducationData', 'root', 'root', {
-  host: "localhost:",
-  port: 8889
-  });
+
+var sequelize = new Sequelize(dbName, dbUser, dbPass, {
+	"host": "localhost"
+});
+
+var teachers = sequelize.define('teachers', {
+    region: Sequelize.STRING(30),
+    num_teachers_thous: Sequelize.FLOAT(11)
+});
+
+
+app.use(express.bodyParser());
+app.get('/teachers', function(req, res) {
+    Teachers.all().success(function(teachers) {
+        res.send(teachers);
+        console.log(teachers);
+        console.log('got to the end');
+    });
+});
+
+try {
+    sequelize.sync();
+}
+catch (err) {
+    console.log("database problem: "+err);  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', routes.index);
