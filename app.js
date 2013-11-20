4caf3594961140949907dff64c23ed5c0d120b34
ryanleeallred <ryanleeallred@gmail.com>
2013-11-20 00:44:09 -0700

    commits 12:44 am files

--- app.js
+++ app.js
@@ -36,18 +36,72 @@ var sequelize = new Sequelize(dbName, dbUser, dbPass, {
 	"host": "localhost"
 });
 
-var teachers = sequelize.define('teachers', {
-    region: Sequelize.STRING(30),
-    num_teachers_thous: Sequelize.FLOAT(11)
+var states = sequelize.define('States', {
+    Region: Sequelize.STRING(30),
+    Population: Sequelize.INTEGER(11),
+    pop_dens: Sequelize.FLOAT,
+    math_4th_basic: Sequelize.INTEGER(11),
+    math_4th_proficient: Sequelize.INTEGER(11),
+    math_8th_basic: Sequelize.INTEGER(11),
+    math_8th_proficient: Sequelize.INTEGER(11),
+    reading_4th_basic: Sequelize.INTEGER(11),
+    reading_4th_proficient: Sequelize.INTEGER(11),
+    reading_8th_basic: Sequelize.INTEGER(11),
+    reading_8th_proficient: Sequelize.INTEGER(11),
+    FY_2011: Sequelize.INTEGER(11),
+    FY_2010: Sequelize.INTEGER(11),
+    Change_2010_2011: Sequelize.FLOAT,
+    perc_change: Sequelize.FLOAT,
+    num_teachers_thous: Sequelize.FLOAT,
+    teacher_perc_less_bach: Sequelize.FLOAT,
+    teacher_perc_bach: Sequelize.FLOAT,
+    teacher_perc_mast: Sequelize.FLOAT,
+    teacher_perc_edspecialist_or_doc: Sequelize.FLOAT,
+    teach_less_than_3: Sequelize.FLOAT,
+    teach_3_to_9: Sequelize.FLOAT,
+    teach_10_to_20: Sequelize.FLOAT,
+    teach_over_20: Sequelize.FLOAT,
+    class_size_elem: Sequelize.FLOAT,
+    class_size_sec: Sequelize.FLOAT,
+    num_teachers_in_thous: Sequelize.FLOAT,
+    num_teachers_elem: Sequelize.FLOAT,
+    num_teachers_sec: Sequelize.FLOAT,
+    av_sal_thous: Sequelize.FLOAT,
+    av_sal_elem: Sequelize.FLOAT,
+    av_sal_sec: Sequelize.FLOAT,
+    enrollment_perc_k12_08: Sequelize.FLOAT,
+    hours_day_k12_08: Sequelize.FLOAT,
+    enrollment_perc_k12_12: Sequelize.FLOAT,
+    hours_day_k12_12: Sequelize.FLOAT,
+    days_year_k12_12: Sequelize.INTEGER(11),
+    hours_year_k12_12: Sequelize.INTEGER(11),
+    enrollment_perc_elem_12: Sequelize.FLOAT,
+    hours_day_elem_12: Sequelize.FLOAT,
+    enrollment_perc_sec_12: Sequelize.FLOAT,
+    hours_day_sec_12: Sequelize.FLOAT,
+    completion_percent: Sequelize.FLOAT,
+    comp_perc_disabilities: Sequelize.FLOAT,
+    comp_perc_limited_english: Sequelize.FLOAT,
+    comp_perc_econ_disadv: Sequelize.FLOAT,
+    Perc_highschoolgrad_25_plus: Sequelize.FLOAT,
+    perc_bach_degree_25_plus: Sequelize.FLOAT,
+    Perc_Advanced_degree_25_plus: Sequelize.FLOAT,
 });
 
 
 app.use(express.bodyParser());
-app.get('/teachers', function(req, res) {
-    Teachers.all().success(function(teachers) {
-        res.send(teachers);
-        console.log(teachers);
-        console.log('got to the end');
+
+app.use(function(req,res,next){
+	states.all().success(function(states) {
+        res.locals.states = states;
+    });
+  next();
+});
+
+app.get('/states', function(req, res) {
+    states.all().success(function(states) {
+        res.send(states);
+        console.log(states);
     });
 });
 
@@ -58,29 +112,24 @@ catch (err) {
     console.log("database problem: "+err);  
 }
 
+// var localStates = null;
+// $.get('/states', function(data) {
+// 	localstates = data;	
+// });
+// localstates = data;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// var col = $()
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/getData',function(req, res) {
 
-	for(var i =0; i < DataTemplate.statesData.length; i++){
+	// for(var i =0; i < DataTemplate.statesData.length; i++){
 
-	}
-	res.send();
+	// }
+	// res.send();
+
+	res.render(index.ejs);
 });
 
 http.createServer(app).listen(app.get('port'), function(){
